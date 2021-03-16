3ebb40c9efdca472ccc2c577b5ff3e5eab2a990f
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-03-16 23:24:01 +0900

    nptypingを導入し、型を明記する

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
+from nptyping import NDArray, Float64
 
 a = np.array([1, 2, 3])
-print(a * 2)
-b = np.array([0, 10, 9])
-print(a + b)
-print(a * b)
-print(np.dot(a, b))
+b = np.array([4, 5, 6])
+
+def add(left: NDArray[np.float64], right: NDArray[np.float64]) -> NDArray[np.float64]:
+	print(left + right)
+
+def multiple(arr: NDArray[np.float64], scalar: Float64) -> NDArray[np.float64]:
+	print(arr * scalar)
+
+add(a, b)
+multiple(a, 10)
