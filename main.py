d2c60d0373eca5dc98b62d76a0e7bfaf3d30a99d
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-03-17 00:56:22 +0900

    numpyで遊んでみた

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def add(left: NDArray[np.float64], right: NDArray[np.float64]) -> NDArray[np.flo
 def multiple(arr: NDArray[np.float64], scalar: Float64) -> NDArray[np.float64]:
 	print(arr * scalar)
 
+def hadamard(left: NDArray[np.float64], right: NDArray[np.float64]) -> NDArray[np.float64]:
+	print(left * right)
+
+def dot(left: NDArray[np.float64], right: NDArray[np.float64]) -> NDArray[np.float64]:
+	print(np.dot(left, right))
+
 add(a, b)
 multiple(a, 10)
+hadamard(a, b)
+dot(a, b)
+
+c = np.array([a, b])
+print(c)
